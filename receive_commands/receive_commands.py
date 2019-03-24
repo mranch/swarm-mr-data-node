3014e279b7e296bf49740309b5935854f81328e1
Valentyn Khoroshchak <smartgtsua@gmail.com>
2019-03-24 12:42:14 +0200

    Added map function

--- receive_commands/receive_commands.py
+++ receive_commands/receive_commands.py
@@ -12,8 +12,31 @@ import os
 
 def make_file(path):
     dir_name = path.split('\\')[-1]
-    if not os.path.isdir('data\\'+dir_name):
-        os.makedirs('data\\'+dir_name)
+    if not os.path.isdir('data\\' + dir_name):
+        os.makedirs('data\\' + dir_name)
+
 
 def write(content):
     pass
+
+
+def map(field_delimiter, key, source, dest):
+    content = open(source).readlines()
+    key_list = key.split(',')
+    res = list()
+    for field in content:
+        line = field.split(field_delimiter)
+        res_line = str()
+        for item in key_list:
+            res_line += line[int(item)]
+            res_line += field_delimiter
+        res_line = res_line[:-1] + '^'
+        for item in field:
+            res_line += item
+        res.append(res_line)
+    open(dest, 'w+').writelines(res)
+    return res
+
+
+map('|', '0', "C:\\Users\\smart\\workspace\\client_data\\text.txt",
+    "C:\\Users\\smart\\workspace\\client_data\\test_out.txt")
