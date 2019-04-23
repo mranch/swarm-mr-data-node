1f141d559575d975c6a9dbf0f8b04d8c757a3f1b
Valentin Khoroshchak <smartgtsua@gmail.com>
2019-04-24 00:49:57 +0300

    Refactoring and added default Reduce method

--- main.py
+++ main.py
@@ -5,55 +5,55 @@ from receive_commands import receive_commands as rc
 from http_communication import shuffle
 import os
 
+
 class Handler(server.BaseHTTPRequestHandler):
-    def do_POST(self):
-        self.send_response(200)
-        self.end_headers()
-        body_length = int(self.headers['content-length'])
-        request_body_json_string = self.rfile.read(body_length).decode('utf-8')
+	def do_POST(self):
+		self.send_response(200)
+		self.end_headers()
+		body_length = int(self.headers['content-length'])
+		request_body_json_string = self.rfile.read(body_length).decode('utf-8')
 
-        # Printing  some info to the server console
-        print('Server on port ' + str(self.server.server_port) + ' - request body: ' + request_body_json_string)
+		# Printing  some info to the server console
+		print('Server on port ' + str(self.server.server_port) + ' - request body: ' + request_body_json_string)
 
-        json_data_obj = json.loads(request_body_json_string)
-        json_data_obj['SEEN_BY_THE_SERVER'] = 'Yes'
+		json_data_obj = json.loads(request_body_json_string)
+		json_data_obj['SEEN_BY_THE_SERVER'] = 'Yes'
 
-        print(request_body_json_string)
-        #rc.make_file(json_data_obj["file_name"])
+		print(request_body_json_string)
 
-        self.wfile.write(bytes(json.dumps(self.recognize_command(json_data_obj)), 'utf-8'))
+		self.wfile.write(bytes(json.dumps(self.recognize_command(json_data_obj)), 'utf-8'))
 
+	def recognize_command(self, content):
+		json_data_obj = dict()
+		if 'make_file' in content:
+			json_data_obj = content['make_file']
+			print(rc.make_file(json_data_obj["file_name"]))
+		elif 'write' in content:
+			json_data_obj = content['write']
+			print(rc.write(json_data_obj))
+		elif 'map' in content:
+			json_data_obj = content['map']
+			rc.map(json_data_obj['mapper'], json_data_obj['field_delimiter'], json_data_obj['key_delimiter'],
+				   json_data_obj['destination_file'])
+			rc.min_max_hash(rc.hash_keys(json_data_obj['destination_file']), json_data_obj['destination_file'])
+		elif 'shuffle' in content:
+			shuffle.shuffle(content['shuffle'])
+		elif 'reduce' in content:
+			rc.reduce('reducer',content['reduce'])
+		return json_data_obj
 
-    def recognize_command(self,content):
-        json_data_obj = dict()
-        #print(content)
-        if 'make_file' in content:
-            json_data_obj = content['make_file']
-            print(rc.make_file(json_data_obj["file_name"]))
-        elif 'write' in content:
-            json_data_obj = content['write']
-            print(rc.write(json_data_obj))
-        elif 'map' in content:
-            json_data_obj = content['map']
-            rc.map(json_data_obj['mapper'], json_data_obj['field_delimiter'], json_data_obj['key_delimiter'],
-                         json_data_obj['destination_file'])
-            rc.min_max_hash(rc.hash_keys(json_data_obj['destination_file']),json_data_obj['destination_file'])
-        elif 'shuffle' in content:
-            print(content['shuffle'])
-            shuffle.shuffle(content['shuffle'])
-        return json_data_obj
 
 def start_server(server_address):
-    my_server = server.ThreadingHTTPServer(server_address, Handler)
-    print(str(server_address) + ' Waiting for POST requests...')
-    my_server.serve_forever()
+	my_server = server.ThreadingHTTPServer(server_address, Handler)
+	print(str(server_address) + ' Waiting for POST requests...')
+	my_server.serve_forever()
 
 
 def start_local_server_on_port(port):
-    p = Process(target=start_server, args=(('127.0.0.1', port),))
-    p.start()
+	p = Process(target=start_server, args=(('127.0.0.1', port),))
+	p.start()
 
 
 if (__name__ == '__main__'):
-    start_local_server_on_port(8014)
-    #start_local_server_on_port(8015)
+	start_local_server_on_port(8014)
+	# start_local_server_on_port(8015)
--- receive_commands/receive_commands.py
+++ receive_commands/receive_commands.py
@@ -1,11 +1,11 @@
-'''
+"""
 File where data node receives requests from management node about:
 1) file(segment) creation
 2) map() method start
 3) shuffle() method start (where necessary)
 4) reduce() method start
 5) request to return a status(if works) and file size
-'''
+"""
 # # TODO: define methods which implement each request separately
 import os
 import json
@@ -13,122 +13,100 @@ import requests
 
 
 def hash_f(str):
-    res = 545
-    for i in str:
-        res += ord(i)
-    return res
+	res = 545
+	for i in str:
+		res += ord(i)
+	return res
 
 
 def make_file(path):
-    dir_name = path.split(os.sep)[-1]
-    if not os.path.isdir(os.path.join('data', dir_name)):
-        os.makedirs(os.path.join('data', dir_name))
+	dir_name = path.split(os.sep)[-1]
+	if not os.path.isdir(os.path.join('data', dir_name)):
+		os.makedirs(os.path.join('data', dir_name))
 
 
 def write(content):
-    dir_name = content['file_name'].split(os.sep)[-2]
-    file_name = content['file_name'].split(os.sep)[-1]
-    path = os.path.join(os.path.dirname(__file__), '..', 'data', dir_name, file_name)
+	dir_name = content['file_name'].split(os.sep)[-2]
+	file_name = content['file_name'].split(os.sep)[-1]
+	path = os.path.join(os.path.dirname(__file__), '..', 'data', dir_name, file_name)
 
-    # if os.path.isdir('\\data\\' + dir_name):
-    f = open(path, 'w+')
-    f.writelines(content['segment'])
-    f.close()
+	# if os.path.isdir('\\data\\' + dir_name):
+	f = open(path, 'w+')
+	f.writelines(content['segment'])
+	f.close()
 
 
-# def map(mapper, field_delimiter, key, dest):
-#     LOC = """
-# def map(field_delimiter, key, dest):
-#     aa = dest.split('\\\\')[-1]
-#     dir_name = "data\\\\"+ aa
-#     for file in os.listdir(dir_name):
-#         content = open(dir_name+'\\\\'+ file).readlines()
-#     dir_name = 'data\\'+ dest.split(os.sep)[-1]
-#     for file in os.listdir(dir_name):
-#         content = open(dir_name+'\\'+ file).readlines()
-#         key_list = key.split(',')
-#         res = list()
-#         for field in content:
-#             field=field[:-1]
-#             line = field.split(field_delimiter)
-#             res_line = str()
-#             for item in key_list:
-#                 res_line += line[int(item)]
-#                 res_line += field_delimiter
-#             res_line = res_line[:-1] + '^'
-#             for item in field:
-#                 res_line += item
-#             res.append(res_line+'\\n')
-#         open(dir_name+'\\\\'+ file, 'w+').writelines(res)
-# map(field_delimiter, key, dest)
-# """
-#     exec(LOC)
+def reduce(reducer, content):
+	import collections
+	result_dict = collections.Counter()
+	dir_name = os.path.join(os.path.dirname(__file__), '..', 'data', content.split(os.sep)[-1])
+	for file in os.listdir(dir_name):
+		content = open(os.path.join(dir_name, file)).readlines()
+		for field in content:
+			field = field[:-1]
+			for item in content:
+				sum = 0
+				for i in item.split('^')[1]:
+					sum += ord(i)
+				result_dict[item.split('^')[0]] += sum
+	result = list()
+	for item in result_dict.keys():
+		result.append(item + '^' + str(result_dict[item]))
+
+	return result
+
 
 def map(mapper, field_delimiter, key, dest):
-    dir_name = os.path.join('data',dest.split(os.sep)[-1])
-    for file in os.listdir(dir_name):
-        content = open(os.path.join(dir_name, file)).readlines()
-        key_list = key.split(',')
-        res = list()
-        for field in content:
-            field=field[:-1]
-            line = field.split(field_delimiter)
-            res_line = str()
-            for item in key_list:
-                res_line += line[int(item)]
-                res_line += field_delimiter
-            res_line = res_line[:-1] + '^'
-            for item in field:
-                res_line += item
-            res.append(res_line+'\n')
-        open(os.path.join(dir_name, file), 'w+').writelines(res)
+	dir_name = os.path.join('data', dest.split(os.sep)[-1])
+	for file in os.listdir(dir_name):
+		content = open(os.path.join(dir_name, file)).readlines()
+		key_list = key.split(',')
+		res = list()
+		for field in content:
+			field = field[:-1]
+			line = field.split(field_delimiter)
+			res_line = str()
+			for item in key_list:
+				res_line += line[int(item)]
+				res_line += field_delimiter
+			res_line = res_line[:-1] + '^'
+			for item in field:
+				res_line += item
+			res.append(res_line + '\n')
+		open(os.path.join(dir_name, file), 'w+').writelines(res)
 
 
 def hash_keys(content):
-    dir_name = content.split(os.sep)[-1]
-    path = os.path.join(os.path.dirname(__file__), '..', 'data', dir_name)
-    print(path)
-    files = []
-    hash_key_list = list()
-    # r=root, d=directories, f = files
-    for r, d, f in os.walk(path):
-        for file in f:
-            files.append(os.path.join(r, file))
-    for f in files:
-        for line in open(f):
-            hash_key_list.append(hash_f(line.split('^')[0]))
-    print(hash_key_list)
-    return hash_key_list
+	dir_name = content.split(os.sep)[-1]
+	path = os.path.join(os.path.dirname(__file__), '..', 'data', dir_name)
+	print(path)
+	files = []
+	hash_key_list = list()
+	# r=root, d=directories, f = files
+	for r, d, f in os.walk(path):
+		for file in f:
+			files.append(os.path.join(r, file))
+	for f in files:
+		for line in open(f):
+			hash_key_list.append(hash_f(line.split('^')[0]))
+	print(hash_key_list)
+	return hash_key_list
 
 
 def min_max_hash(hash_key_list, file_name):
-    arbiter_node_json_data = open(os.path.join('config', 'data_node_info.json'))
-    arbiter_node_data = json.load(arbiter_node_json_data)['arbiter_address']
-    print(arbiter_node_data)
-    res = list()
-    res.append(max(hash_key_list))
-    res.append(min(hash_key_list))
-    url = 'http://' + arbiter_node_data
-    diction = {
-        'hash':
-            {
-                'list_keys': res,
-                'file_name': file_name
-            }
-    }
-    response = requests.post(url, data=json.dumps(diction))
-    return response.json()
-    # return res
-
-# Python program to illustrate use of exec to
-# execute a given code as string.
-
-
-# map('|', '0', "C:\\Users\\smart\\workspace\\client_data\\text.txt",
-#     "C:\\Users\\smart\\workspace\\client_data\\test_out.txt")
-
-# write({"file_name": 'C:/Users/smart/workspace/KURSOVA/swarm-mr-client\\..\\..\\client_data\\out.txt\\f2', "segment": ['A|1', 'B|2']})
-
-# f = open(os.path.join(os.pardir + '\\data\\', "filename"), "w")
-# f.write('hhhhh')
-# f.close()
+	arbiter_node_json_data = open(os.path.join('config', 'data_node_info.json'))
+	arbiter_node_data = json.load(arbiter_node_json_data)['arbiter_address']
+	print(arbiter_node_data)
+	res = list()
+	res.append(max(hash_key_list))
+	res.append(min(hash_key_list))
+	url = 'http://' + arbiter_node_data
+	diction = {
+		'hash':
+			{
+				'list_keys': res,
+				'file_name': file_name
+			}
+	}
+	response = requests.post(url, data=json.dumps(diction))
+	return response.json()
