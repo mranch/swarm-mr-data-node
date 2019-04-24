89d6e630a52e9ddfa3ad7752a2c3cc5763eb8170
mranch <Anchiforov@gmail.com>
2019-04-24 18:28:26 +0300

    Added additional folder for mapped files

--- http_communication/shuffle.py
+++ http_communication/shuffle.py
@@ -9,40 +9,42 @@ from receive_commands.receive_commands import hash_f
 
 
 def shuffle(content):
-	dir_name = content['file_name'].split(os.sep)[-1]
-	path = os.path.join(os.path.dirname(__file__), '..', 'data', dir_name)
-	arbiter_node_json_data = open(os.path.join('config', 'data_node_info.json'))
-	arbiter_node_data = json.load(arbiter_node_json_data)['self_address']
-	files = []
-	list_of_lines = list()
-	result = {
-		'shuffle_items': [
-		]
-	}
-	for i in content['nodes_keys']:
-		result['shuffle_items'].append({'data_node_ip': i['data_node_ip'], 'content': []})
-	# r=root, d=directories, f = files
+    dir_name = content['file_name'].split(os.sep)[-1]
+    path = os.path.join(os.path.dirname(__file__), '..', 'data', dir_name)
+    arbiter_node_json_data = open(os.path.join('config', 'data_node_info.json'))
+    self_node_ip = json.load(arbiter_node_json_data)['self_address']
+    files = []
+    list_of_lines = list()
+    result = {
+        'shuffle_items': [
+        ]
+    }
 
-	for r, d, f in os.walk(path):
-		for file in f:
-			files.append(os.path.join(r, file))
-	for f in files:
-		for line in open(f):
+    for i in content['nodes_keys']:
+        if i['data_node_ip'] != self_node_ip:
+            result['shuffle_items'].append({'data_node_ip': i['data_node_ip'], 'content': []})
+    # r=root, d=directories, f = files
 
-			for item in content['nodes_keys']:
-				if item['hash_keys_range'][1] == content['max_hash']:
-					if item['hash_keys_range'][0] <= hash_f(line.split('^')[0]) and hash_f(line.split('^')[0]) <= \
-							item['hash_keys_range'][1]:
+    for r, d, f in os.walk(path):
+        for file in f:
+            files.append(os.path.join(r, file))
+    for f in files:
+        for line in open(f):
 
-						for i in result['shuffle_items']:
-							if i['data_node_ip'] == item['data_node_ip']:
-								i['content'].append(line)
+            for item in content['nodes_keys']:
+                if item['hash_keys_range'][1] == content['max_hash']:
+                    if item['hash_keys_range'][0] <= hash_f(line.split('^')[0]) <= item['hash_keys_range'][1]:
 
-				else:
-					if item['hash_keys_range'][0] <= hash_f(line.split('^')[0]) and hash_f(line.split('^')[0]) < \
-							item['hash_keys_range'][1]:
-						for i in result['shuffle_items']:
-							if i['data_node_ip'] == item['data_node_ip']:
-								i['content'].append(line)
+                        for i in result['shuffle_items']:
+                            if i['data_node_ip'] == item['data_node_ip']:
+                                i['content'].append(line)
 
-# shuffle({'nodes_keys': [{'data_node_ip': '127.0.0.1:8014', 'hash_keys_range': [612, 618.5]}, {'data_node_ip': '127.0.0.1:8015', 'hash_keys_range': [618.5, 625.0]}], 'max_hash': 625, 'file_name':'.\\..\\client_data\\out.txt'})
+                else:
+                    if item['hash_keys_range'][0] <= hash_f(line.split('^')[0]) < item['hash_keys_range'][1]:
+                        for i in result['shuffle_items']:
+                            if i['data_node_ip'] == item['data_node_ip']:
+                                i['content'].append(line)
+    print(result)
+# shuffle({'nodes_keys': [{'data_node_ip': '127.0.0.1:8014', 'hash_keys_range': [612, 618.5]},
+# {'data_node_ip': '127.0.0.1:8015', 'hash_keys_range': [618.5, 625.0]}],
+# 'max_hash': 625, 'file_name':'.\\..\\client_data\\out.txt'})
--- receive_commands/receive_commands.py
+++ receive_commands/receive_commands.py
@@ -13,100 +13,105 @@ import requests
 
 
 def hash_f(str):
-	res = 545
-	for i in str:
-		res += ord(i)
-	return res
+    res = 545
+    for i in str:
+        res += ord(i)
+    return res
 
 
 def make_file(path):
-	dir_name = path.split(os.sep)[-1]
-	if not os.path.isdir(os.path.join('data', dir_name)):
-		os.makedirs(os.path.join('data', dir_name))
+    dir_name = path.split(os.sep)[-1]
+    if not os.path.isdir(os.path.join('data', dir_name)):
+        os.makedirs(os.path.join('data', dir_name))
 
 
 def write(content):
-	dir_name = content['file_name'].split(os.sep)[-2]
-	file_name = content['file_name'].split(os.sep)[-1]
-	path = os.path.join(os.path.dirname(__file__), '..', 'data', dir_name, file_name)
+    dir_name = content['file_name'].split(os.sep)[-2]
 
-	# if os.path.isdir('\\data\\' + dir_name):
-	f = open(path, 'w+')
-	f.writelines(content['segment'])
-	f.close()
+    file_name = content['file_name'].split(os.sep)[-1]
+    path = os.path.join(os.path.dirname(__file__), '..', 'data', dir_name, file_name)
+
+    # if os.path.isdir('\\data\\' + dir_name):
+    f = open(path, 'w+')
+    f.writelines(content['segment'])
+    f.close()
 
 
 def reduce(reducer, content):
-	import collections
-	result_dict = collections.Counter()
-	dir_name = os.path.join(os.path.dirname(__file__), '..', 'data', content.split(os.sep)[-1])
-	for file in os.listdir(dir_name):
-		content = open(os.path.join(dir_name, file)).readlines()
-		for field in content:
-			field = field[:-1]
-			for item in content:
-				sum = 0
-				for i in item.split('^')[1]:
-					sum += ord(i)
-				result_dict[item.split('^')[0]] += sum
-	result = list()
-	for item in result_dict.keys():
-		result.append(item + '^' + str(result_dict[item]))
+    import collections
+    result_dict = collections.Counter()
+    dir_name = os.path.join(os.path.dirname(__file__), '..', 'data', content.split(os.sep)[-1])
+    for file in os.listdir(dir_name):
+        content = open(os.path.join(dir_name, file)).readlines()
+        for field in content:
+            field = field[:-1]
+            for item in content:
+                sum = 0
+                for i in item.split('^')[1]:
+                    sum += ord(i)
+                result_dict[item.split('^')[0]] += sum
+    result = list()
+    for item in result_dict.keys():
+        result.append(item + '^' + str(result_dict[item]))
 
-	return result
+    return result
 
 
 def map(mapper, field_delimiter, key, dest):
-	dir_name = os.path.join('data', dest.split(os.sep)[-1])
-	for file in os.listdir(dir_name):
-		content = open(os.path.join(dir_name, file)).readlines()
-		key_list = key.split(',')
-		res = list()
-		for field in content:
-			field = field[:-1]
-			line = field.split(field_delimiter)
-			res_line = str()
-			for item in key_list:
-				res_line += line[int(item)]
-				res_line += field_delimiter
-			res_line = res_line[:-1] + '^'
-			for item in field:
-				res_line += item
-			res.append(res_line + '\n')
-		open(os.path.join(dir_name, file), 'w+').writelines(res)
+    dir_name = os.path.join('data', dest.split(os.sep)[-1])
+    new_dir_name = dir_name.split('\\')[-1].split('.')[0] \
+                   + '_map' + '.' + dir_name.split('\\')[-1].split('.')[-1]
+    make_file(new_dir_name)
+    for file in os.listdir(dir_name):
+        content = open(os.path.join(dir_name, file)).readlines()
+        key_list = key.split(',')
+        res = list()
+        for field in content:
+            field = field[:-1]
+            line = field.split(field_delimiter)
+            res_line = str()
+            for item in key_list:
+                res_line += line[int(item)]
+                res_line += field_delimiter
+            res_line = res_line[:-1] + '^'
+            for item in field:
+                res_line += item
+            res.append(res_line + '\n')
+        f = open(os.path.join(os.path.dirname(__file__), '..', 'data', new_dir_name, file), 'w+')
+        f.writelines(res)
 
 
 def hash_keys(content):
-	dir_name = content.split(os.sep)[-1]
-	path = os.path.join(os.path.dirname(__file__), '..', 'data', dir_name)
-	print(path)
-	files = []
-	hash_key_list = list()
-	# r=root, d=directories, f = files
-	for r, d, f in os.walk(path):
-		for file in f:
-			files.append(os.path.join(r, file))
-	for f in files:
-		for line in open(f):
-			hash_key_list.append(hash_f(line.split('^')[0]))
-	print(hash_key_list)
-	return hash_key_list
+    dir_name = content.split(os.sep)[-1]
+    path = os.path.join(os.path.dirname(__file__), '..', 'data', dir_name)
+    print(path)
+    files = []
+    hash_key_list = list()
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(path):
+        for file in f:
+            files.append(os.path.join(r, file))
+    for f in files:
+        for line in open(f):
+            hash_key_list.append(hash_f(line.split('^')[0]))
+    print(hash_key_list)
+    return hash_key_list
 
 
 def min_max_hash(hash_key_list, file_name):
-	arbiter_node_json_data = open(os.path.join('config', 'data_node_info.json'))
-	arbiter_node_data = json.load(arbiter_node_json_data)['arbiter_address']
-	print(arbiter_node_data)
-	res = list()
-	res.append(max(hash_key_list))
-	res.append(min(hash_key_list))
-	url = 'http://' + arbiter_node_data
-	diction = {
-		'hash':
-			{
-				'list_keys': res,
-				'file_name': file_name
-			}
-	}
-	response = requests.post(url, data=json.dumps(diction))
-	return response.json()
+    arbiter_node_json_data = open(os.path.join('config', 'data_node_info.json'))
+    arbiter_node_data = json.load(arbiter_node_json_data)['arbiter_address']
+    print(arbiter_node_data)
+    res = list()
+    res.append(max(hash_key_list))
+    res.append(min(hash_key_list))
+    url = 'http://' + arbiter_node_data
+    diction = {
+        'hash':
+            {
+                'list_keys': res,
+                'file_name': file_name
+            }
+    }
+    response = requests.post(url, data=json.dumps(diction))
+    return response.json()
